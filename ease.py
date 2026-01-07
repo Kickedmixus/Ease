45f1e890c2008606272f43340570a6d21e7f20e7
Kickedmixus <michael.hizalev@gmail.com>
2026-01-06 20:49:58 -0800

    v1.0.1a (fixed dumb typo)

--- ease.py
+++ ease.py
@@ -2,43 +2,43 @@
 
 import math
 
-def easeOut(easetype,start,end,lenght):
+def easeOut(easetype,start,end,length):
 
     if easetype == "linear":
-        add = (end-start)/lenght
+        add = (end-start)/length
         graph = [start]
 
-        for i in range(lenght):
+        for i in range(length):
             graph.append(graph[i]+add)
 
     elif easetype == "sine":
-        add = (end-start)/lenght
+        add = (end-start)/length
         graph = []
 
-        for i in range(lenght):
-            graph.append((math.sin(((i+1)*math.pi)/(lenght*2))*(end-start))+start)
+        for i in range(length):
+            graph.append((math.sin(((i+1)*math.pi)/(length*2))*(end-start))+start)
 
     print (len(graph))
 
     return graph
 
-def easeIn(easetype,start,end,lenght):
+def easeIn(easetype,start,end,length):
 
-    easein = easeOut(easetype,start,end,lenght)[::-1]
+    easein = easeOut(easetype,start,end,length)[::-1]
     graph = []
 
-    for i in range(lenght):
+    for i in range(length):
         graph.append(end - easein[i])
 
     return graph
 
-def easeInOut(easetype,start,end,lenght):
+def easeInOut(easetype,start,end,length):
 
-    easein = easeIn(easetype,start+((end-start)/2),end,int(lenght/2))
-    easeout = easeOut(easetype,start,end-((end-start)/2),int(lenght/2))
+    easein = easeIn(easetype,start+((end-start)/2),end,int(length/2))
+    easeout = easeOut(easetype,start,end-((end-start)/2),int(length/2))
 
-    for i in range(int(lenght/2)):
+    for i in range(int(length/2)):
         easeout[i] += (end-start)/2
 
-    if lenght/2 == round(lenght/2):
+    if length/2 == round(length/2):
         return easein + easeout
